8587af465e2c2c34cd98cf19e81b79bcfae09749
VitorFelisbert <vitorafs22@gmail.com>
2025-12-16 20:53:20 -0300

    aprimorando

--- js/js.js
+++ js/js.js
@@ -1,24 +1,27 @@
-fetch(`banco-dados.json`).then(response => response.json()).then(data => {
-    const container = document.createElement('tr');
-    data.manga .forEach( manga =>
+fetch(`../json/banco-dados.json`).then(response => response.json()).then(data => {
+    const mangaOnline = document.querySelector('#mangaOnline');
+ 
+    data .forEach( manga =>
     {
-        const mangasOnline = document.createElement('div');
-        mangasOnline.innerHTML = `
+        mangaOnline.innerHTML = `
         <td style="max-width: 500px;">
         <h1>${manga.nome}</h1>
         <a href="${manga.img}">
+        <div class="divimagem">
+        <img src="img/One-Piece.jpg" alt="One piece" ></div>
         <p>${manga.author}</p>
+        <div class="nota populares">
         <p style="color:yellow; display: inline;"> ${manga.nota} ★</p></div>
         <p>${manga.descricao} </p>
-        <h2>${capitulos}</h2>  
+        <h2>${manga.capitulos}</h2>  
         </a>
         </td>
         `;
 
+
     }
 
     )
-    const mangaOnline = document.querySelector('#mangaOnline');
-    mangaOnline.appendChild(container)
+    
 }
 )
