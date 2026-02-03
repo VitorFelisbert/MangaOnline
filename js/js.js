6458976dc552699b393fed9b47c4a3fa603fc70a
Arthur <arthurmoney21@gmail.com>
2026-02-03 18:12:15 -0300

    reeditando

--- js/js.js
+++ js/js.js
@@ -3,25 +3,23 @@ fetch(`../json/banco-dados.json`).then(response => response.json()).then(data =>
  
     data .forEach( manga =>
     {
-        mangaOnline.innerHTML = `
+        mangaOnline.innerHTML += `      
         <td style="max-width: 500px;">
-        <h1>${manga.nome}</h1>
-        <a href="${manga.img}">
+        
         <div class="divimagem">
-        <img src="img/One-Piece.jpg" alt="One piece" ></div>
-        <p>${manga.author}</p>
-        <div class="nota populares">
+        <img src="${manga.img}" alt="" ></div>
+        <p>${manga.nome}</p>
+        <p>${manga.autor}</p>
+        <div class="notapopulares">
         <p style="color:yellow; display: inline;"> ${manga.nota} ★</p></div>
-        <p>${manga.descricao} </p>
-        <h2>${manga.capitulos}</h2>  
-        </a>
+        <p class="mangs">${manga.descricao} </p>
+        <h2>${manga.capitulos}</h2> 
+        
         </td>
         `;
 
-
-    }
-
-    )
+    }) 
+  
     
 }
 )
